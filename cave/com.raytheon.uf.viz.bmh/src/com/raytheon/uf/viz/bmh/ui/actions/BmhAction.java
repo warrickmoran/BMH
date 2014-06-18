9795304aed84d90ea9926f16343e798f3608b24b
Mike Duff <michael.duff@raytheon.com>
2014-06-18 12:18:09 -0500

    Omaha #3188 - Initial checkin of main bmh menu and starter dialog.
    Change-Id: Ibdcb8df0e861eca804573d414b7bc18f1ae346df

---
+++ cave/com.raytheon.uf.viz.bmh/src/com/raytheon/uf/viz/bmh/ui/actions/BmhAction.java
@@ -0,0 +1,57 @@
+/**
+ * This software was developed and / or modified by Raytheon Company,
+ * pursuant to Contract DG133W-05-CQ-1067 with the US Government.
+ * 
+ * U.S. EXPORT CONTROLLED TECHNICAL DATA
+ * This software product contains export-restricted data whose
+ * export/transfer/disclosure is restricted by U.S. law. Dissemination
+ * to non-U.S. persons whether in the United States or abroad requires
+ * an export license or other authorization.
+ * 
+ * Contractor Name:        Raytheon Company
+ * Contractor Address:     6825 Pine Street, Suite 340
+ *                         Mail Stop B8
+ *                         Omaha, NE 68106
+ *                         402.291.0100
+ * 
+ * See the AWIPS II Master Rights File ("Master Rights File.pdf") for
+ * further licensing information.
+ **/
+package com.raytheon.uf.viz.bmh.ui.actions;
+
+import org.eclipse.core.commands.AbstractHandler;
+import org.eclipse.core.commands.ExecutionEvent;
+import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.swt.widgets.Shell;
+import org.eclipse.ui.PlatformUI;
+
+import com.raytheon.uf.viz.bmh.ui.dialogs.BmhMainDlg;
+
+/**
+ * TODO Add Description
+ * 
+ * <pre>
+ * 
+ * SOFTWARE HISTORY
+ * 
+ * Date         Ticket#    Engineer    Description
+ * ------------ ---------- ----------- --------------------------
+ * Jun 18, 2014            mpduff     Initial creation
+ * 
+ * </pre>
+ * 
+ * @author mpduff
+ * @version 1.0
+ */
+
+public class BmhAction extends AbstractHandler {
+
+    @Override
+    public Object execute(ExecutionEvent event) throws ExecutionException {
+        Shell shell = PlatformUI.getWorkbench().getActiveWorkbenchWindow()
+                .getShell();
+        BmhMainDlg main = new BmhMainDlg(shell);
+        main.open();
+        return null;
+    }
+}
---
+++ cave/com.raytheon.uf.viz.bmh/src/com/raytheon/uf/viz/bmh/ui/dialogs/BmhMainDlg.java
@@ -0,0 +1,65 @@
+/**
+ * This software was developed and / or modified by Raytheon Company,
+ * pursuant to Contract DG133W-05-CQ-1067 with the US Government.
+ * 
+ * U.S. EXPORT CONTROLLED TECHNICAL DATA
+ * This software product contains export-restricted data whose
+ * export/transfer/disclosure is restricted by U.S. law. Dissemination
+ * to non-U.S. persons whether in the United States or abroad requires
+ * an export license or other authorization.
+ * 
+ * Contractor Name:        Raytheon Company
+ * Contractor Address:     6825 Pine Street, Suite 340
+ *                         Mail Stop B8
+ *                         Omaha, NE 68106
+ *                         402.291.0100
+ * 
+ * See the AWIPS II Master Rights File ("Master Rights File.pdf") for
+ * further licensing information.
+ **/
+package com.raytheon.uf.viz.bmh.ui.dialogs;
+
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.SelectionAdapter;
+import org.eclipse.swt.events.SelectionEvent;
+import org.eclipse.swt.widgets.Button;
+import org.eclipse.swt.widgets.Shell;
+
+import com.raytheon.viz.ui.dialogs.CaveSWTDialogBase;
+
+/**
+ * Initial implementation/place holder
+ * 
+ * <pre>
+ * 
+ * SOFTWARE HISTORY
+ * 
+ * Date         Ticket#    Engineer    Description
+ * ------------ ---------- ----------- --------------------------
+ * Jun 18, 2014            mpduff     Initial creation
+ * 
+ * </pre>
+ * 
+ * @author mpduff
+ * @version 1.0
+ */
+
+public class BmhMainDlg extends CaveSWTDialogBase {
+
+    public BmhMainDlg(Shell parentShell) {
+        super(parentShell, SWT.DIALOG_TRIM, CAVE.PERSPECTIVE_INDEPENDENT);
+    }
+
+    @Override
+    protected void initializeComponents(Shell shell) {
+        Button b = new Button(shell, SWT.PUSH);
+        b.setText("BMH...");
+        b.addSelectionListener(new SelectionAdapter() {
+            @Override
+            public void widgetSelected(SelectionEvent e) {
+                System.out.println("Launch BMH");
+            }
+        });
+
+    }
+}
