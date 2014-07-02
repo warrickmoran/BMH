d6ed4293a47f89a58647f8f0273e991a504e7aca
Bryan Kowal <Bryan.Kowal@raytheon.com>
2014-07-02 16:06:07 -0500

    Omaha #3345 - BMH-specific EDEX esb configuration. Automatic deployment will be possible with the updated deploy-install in 15.1.1.

---
+++ edex/deploy.edex-BMH/esb/etc/bmh.sh
@@ -0,0 +1,3 @@
+# TODO: specify required unique ports for the BMH EDEX instance.
+
+export BMH_DATA=/awips2/bmh/data
---
+++ edex/deploy.edex-BMH/opt/db/ddl/bmh/createBMHDB.sql
@@ -0,0 +1,26 @@
+/**
+ * This software was developed and / or modified by Raytheon Company,
+ * pursuant to Contract DG133W-05-CQ-1067 with the US Government.
+ * 
+ * U.S. EXPORT CONTROLLED TECHNICAL DATA
+ * This software product contains export-restricted data whose
+ * export/transfer/disclosure is restricted by U.S. law. Dissemination
+ * to non-U.S. persons whether in the United States or abroad requires
+ * an export license or other authorization.
+ * 
+ * Contractor Name:        Raytheon Company
+ * Contractor Address:     6825 Pine Street, Suite 340
+ *                         Mail Stop B8
+ *                         Omaha, NE 68106
+ *                         402.291.0100
+ * 
+ * See the AWIPS II Master Rights File ("Master Rights File.pdf") for
+ * further licensing information.
+ **/
+\set ON_ERROR_STOP 1
+DROP DATABASE IF EXISTS bmh;
+DROP TABLESPACE IF EXISTS bmh;
+CREATE TABLESPACE bmh owner awips location '/awips2/data/bmh';
+CREATE DATABASE bmh OWNER awips TABLESPACE bmh;
+\connect bmh
+CREATE SCHEMA bmh AUTHORIZATION awips;
