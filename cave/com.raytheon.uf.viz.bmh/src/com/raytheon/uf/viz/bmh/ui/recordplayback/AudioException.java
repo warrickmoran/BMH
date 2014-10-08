f4804b00d9b77355fad71c867c6eac880f96bf01
Bryan Kowal <Bryan.Kowal@raytheon.com>
2014-10-08 14:59:32 -0500

    Omaha #3657 - implemented audio recording and playback within the Record Playback Dialog.
    amend: remove playback thread wait
    
    Change-Id: I1c5a45ef8d03a41b4e5149d1cf373efe4383dea2

---
+++ cave/com.raytheon.uf.viz.bmh/src/com/raytheon/uf/viz/bmh/ui/recordplayback/AudioException.java
@@ -0,0 +1,49 @@
+/**
+ * This software was developed and / or modified by Raytheon Company,
+ * pursuant to Contract DG133W-05-CQ-1067 with the US Government.
+ * 
+ * U.S. EXPORT CONTROLLED TECHNICAL DATA
+ * This software product contains export-restricted data whose
+ * export/transfer/disclosure is restricted by U.S. law. Dissemination
+ * to non-U.S. persons whether in the United States or abroad requires
+ * an export license or other authorization.
+ * 
+ * Contractor Name:        Raytheon Company
+ * Contractor Address:     6825 Pine Street, Suite 340
+ *                         Mail Stop B8
+ *                         Omaha, NE 68106
+ *                         402.291.0100
+ * 
+ * See the AWIPS II Master Rights File ("Master Rights File.pdf") for
+ * further licensing information.
+ **/
+package com.raytheon.uf.viz.bmh.ui.recordplayback;
+
+/**
+ * Indicates that a problem has occurred during live audio processing.
+ * 
+ * <pre>
+ * 
+ * SOFTWARE HISTORY
+ * 
+ * Date         Ticket#    Engineer    Description
+ * ------------ ---------- ----------- --------------------------
+ * Oct 7, 2014  3657       bkowal      Initial creation
+ * 
+ * </pre>
+ * 
+ * @author bkowal
+ * @version 1.0
+ */
+
+public class AudioException extends Exception {
+    private static final long serialVersionUID = 2462327450818779484L;
+
+    /**
+     * @param message the detail message
+     * @param cause the cause
+     */
+    public AudioException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
