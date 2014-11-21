557228df454aac08c779fb476647b3465aef5161
Ben Steffensmeier <Ben.Steffensmeier@raytheon.com>
2014-11-21 15:57:31 -0600

    Omaha #3817 Implement lsitener for system status and some bug fixes/consistency issues.

--- common/com.raytheon.uf.common.bmh/src/com/raytheon/uf/common/bmh/systemstatus/SystemStatusMonitor.java
+++ common/com.raytheon.uf.common.bmh/src/com/raytheon/uf/common/bmh/systemstatus/SystemStatusMonitor.java
@@ -24,12 +24,15 @@ import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Iterator;
 import java.util.Map;
+import java.util.Set;
+import java.util.concurrent.CopyOnWriteArraySet;
 
 import com.raytheon.uf.common.bmh.notify.status.BmhEdexStatus;
 import com.raytheon.uf.common.bmh.notify.status.CommsManagerStatus;
 import com.raytheon.uf.common.bmh.notify.status.DacHardwareStatusNotification;
 import com.raytheon.uf.common.bmh.notify.status.PeriodicStatusMessage;
 import com.raytheon.uf.common.bmh.notify.status.TTSStatus;
+import com.raytheon.uf.common.bmh.systemstatus.ISystemStatusListener.BmhComponent;
 import com.raytheon.uf.common.serialization.annotations.DynamicSerialize;
 import com.raytheon.uf.common.serialization.annotations.DynamicSerializeElement;
 
@@ -67,6 +70,8 @@ public class SystemStatusMonitor {
     @DynamicSerializeElement
     private Map<String, TTSStatus> ttsStatus;
 
+    private transient Set<ISystemStatusListener> listeners = new CopyOnWriteArraySet<>();
+
     public SystemStatusMonitor() {
         this(null);
     }
@@ -85,9 +90,22 @@ public class SystemStatusMonitor {
         }
     }
 
+    /**
+     * @return the status of the dac for all currently connected transmitter
+     *         groups. It is possible to know that a transmitter group is
+     *         connected but still have not have a
+     *         {@link DacHardwareStatusNotification}, in this case the entry in
+     *         the map for that group will have a null value.
+     */
     public Map<String, DacHardwareStatusNotification> getDacStatus() {
-        synchronized (dacStatus) {
-            return new HashMap<>(dacStatus);
+        Set<String> connectedGroups = getConnectedTransmitterGroups();
+        synchronized (this.dacStatus) {
+            Map<String, DacHardwareStatusNotification> dacStatus = new HashMap<>(
+                    connectedGroups.size(), 1.0f);
+            for (String group : connectedGroups) {
+                dacStatus.put(group, this.dacStatus.get(group));
+            }
+            return dacStatus;
         }
     }
 
@@ -96,29 +114,53 @@ public class SystemStatusMonitor {
         this.dacStatus = new HashMap<>(dacStatus);
     }
 
+    /**
+     * @return a map of hostname to the current status of the comms manager
+     *         running on that host.
+     */
     public Map<String, CommsManagerStatus> getCommsStatus() {
-        synchronized (commsStatus) {
-            return new HashMap<>(commsStatus);
+        Map<String, CommsManagerStatus> commsStatus;
+        synchronized (this.commsStatus) {
+            commsStatus = new HashMap<>(this.commsStatus);
         }
+        removeExpired(commsStatus.values());
+        return commsStatus;
     }
 
     public void setCommsStatus(Map<String, CommsManagerStatus> commsStatus) {
         this.commsStatus = new HashMap<>(commsStatus);
     }
 
+    /**
+     * @return a map of hostname to the current status of the edex running on
+     *         that host.
+     */
     public Map<String, BmhEdexStatus> getEdexStatus() {
-        synchronized (edexStatus) {
-            return new HashMap<>(edexStatus);
+        Map<String, BmhEdexStatus> edexStatus;
+        synchronized (this.edexStatus) {
+            edexStatus = new HashMap<>(this.edexStatus);
         }
+        removeExpired(edexStatus.values());
+        return edexStatus;
     }
 
     public void setEdexStatus(Map<String, BmhEdexStatus> edexStatus) {
         this.edexStatus = new HashMap<>(edexStatus);
     }
 
+    /**
+     * @return a map of hostname to the current status of the tts connection for
+     *         the edex on that host.
+     */
     public Map<String, TTSStatus> getTtsStatus() {
-        synchronized (ttsStatus) {
-            return new HashMap<>(ttsStatus);
+        Collection<String> connectedEdices = getEdexStatus().keySet();
+        synchronized (this.ttsStatus) {
+            Map<String, TTSStatus> ttsStatus = new HashMap<>(
+                    connectedEdices.size(), 1.0f);
+            for (String group : connectedEdices) {
+                dacStatus.put(group, this.dacStatus.get(group));
+            }
+            return ttsStatus;
         }
     }
 
@@ -138,103 +180,112 @@ public class SystemStatusMonitor {
         }
     }
 
-    public void updateDacHardwareStatus(DacHardwareStatusNotification dacStatus) {
+    protected void updateDacHardwareStatus(
+            DacHardwareStatusNotification dacStatus) {
+        DacHardwareStatusNotification prev = null;
         synchronized (this.dacStatus) {
-            this.dacStatus.put(dacStatus.getTransmitterGroup(), dacStatus);
+            prev = this.dacStatus.put(dacStatus.getTransmitterGroup(),
+                    dacStatus);
+        }
+        if (prev == null || !prev.equals(dacStatus)) {
+            notifyListeners(BmhComponent.DAC, dacStatus.getTransmitterGroup());
         }
     }
 
-    public void updateEdexStatus(BmhEdexStatus edexStatus) {
+    protected void updateEdexStatus(BmhEdexStatus edexStatus) {
+        BmhEdexStatus prev = null;
         synchronized (this.edexStatus) {
-            this.edexStatus.put(edexStatus.getHost(), edexStatus);
+            prev = this.edexStatus.put(edexStatus.getHost(), edexStatus);
+        }
+        if (prev == null || !prev.equals(edexStatus)) {
+            notifyListeners(BmhComponent.EDEX, edexStatus.getHost());
         }
     }
 
-    private void updateCommsManagerStatus(CommsManagerStatus commsStatus) {
+    protected void updateCommsManagerStatus(CommsManagerStatus commsStatus) {
+        CommsManagerStatus prev = null;
         synchronized (this.commsStatus) {
-            this.commsStatus.put(commsStatus.getHost(), commsStatus);
+            prev = this.commsStatus.put(commsStatus.getHost(), commsStatus);
+        }
+        if (prev == null || !prev.equals(commsStatus)) {
+            notifyListeners(BmhComponent.CommsManager, commsStatus.getHost());
+            // TODO might need to notify of new dacs.
         }
     }
 
-    private void updateTtsStatus(TTSStatus ttsStatus) {
+    protected void updateTtsStatus(TTSStatus ttsStatus) {
+        TTSStatus prev = null;
         synchronized (this.ttsStatus) {
-            this.ttsStatus.put(ttsStatus.getHost(), ttsStatus);
+            prev = this.ttsStatus.put(ttsStatus.getHost(), ttsStatus);
+        }
+        if (prev == null || !prev.equals(ttsStatus)) {
+            notifyListeners(BmhComponent.TTS, ttsStatus.getHost());
         }
     }
 
-    private boolean isGroupConnectedtoComms(String transmitterGroupName) {
-        synchronized (this.commsStatus) {
-            for (CommsManagerStatus commsStatus : this.commsStatus.values()) {
-                if (commsStatus
-                        .containsConnectedTransmitterGroup(transmitterGroupName)) {
-                    if (isExpired(commsStatus)) {
-                        this.commsStatus.remove(commsStatus.getHost());
-                        return false;
-                    } else {
-                        return true;
-                    }
-                }
+    /**
+     * Get all the transmitter groups that are running on dacs that are
+     * currently communicating with a comms manager.
+     * 
+     * @return
+     */
+    public Set<String> getConnectedTransmitterGroups() {
+        Set<String> connectedGroups = new HashSet<>();
+        for (CommsManagerStatus commsStatus : this.getCommsStatus().values()) {
+            Set<String> commsGroups = commsStatus
+                    .getConnectedTransmitterGroups();
+            if (commsGroups != null) {
+                connectedGroups.addAll(commsGroups);
             }
         }
-        return false;
+        return connectedGroups;
     }
 
     public DacHardwareStatusNotification getDacHardwareStatus(
             String transmitterGroupName) {
-        if (isGroupConnectedtoComms(transmitterGroupName)) {
-            synchronized (dacStatus) {
-                return dacStatus.get(transmitterGroupName);
-            }
-        } else {
-            return null;
-        }
+        return getDacStatus().get(transmitterGroupName);
     }
 
     public Collection<CommsManagerStatus> getConnectedCommsManagers() {
-        synchronized (commsStatus) {
-            removeExpired(commsStatus.values());
-            return new HashSet<>(commsStatus.values());
-        }
+        return getCommsStatus().values();
     }
 
     public Collection<BmhEdexStatus> getConnectedEdices() {
-        synchronized (edexStatus) {
-            removeExpired(edexStatus.values());
-            return new HashSet<>(edexStatus.values());
-        }
+        return getEdexStatus().values();
     }
 
-    public boolean isDacConnected(String transmitterGroupName) {
-        synchronized (dacStatus) {
-            return dacStatus.containsKey(transmitterGroupName);
-        }
+    public Collection<TTSStatus> getConnectedTtsHosts() {
+        return getTtsStatus().values();
+    }
+
+    public boolean isTransmitterGroupConnected(String transmitterGroupName) {
+        return getDacStatus().containsKey(transmitterGroupName);
     }
 
     public boolean isCommsManagerConnected(String host) {
-        synchronized (commsStatus) {
-            return commsStatus.containsKey(host);
-        }
+        return getCommsStatus().containsKey(host);
     }
 
     public boolean isEdexConnected(String host) {
-        synchronized (edexStatus) {
-            return edexStatus.containsKey(host);
-        }
+        return getEdexStatus().containsKey(host);
     }
 
     public boolean isTtsConnected(String host) {
-        if (isEdexConnected(host)) {
-            synchronized (ttsStatus) {
-                TTSStatus ttsStatus = this.ttsStatus.get(host);
-                if (ttsStatus != null) {
-                    return ttsStatus.isConnected();
-                } else {
-                    return false;
-                }
-            }
-        } else {
-            return false;
+        return getTtsStatus().containsKey(host);
+    }
+
+    private void notifyListeners(BmhComponent component, String key) {
+        for (ISystemStatusListener listener : listeners) {
+            listener.systemStatusChanged(component, key);
         }
+    }
+
+    public void addListener(ISystemStatusListener listener) {
+        listeners.add(listener);
+    }
+
+    public void removeListener(ISystemStatusListener listener) {
+        listeners.remove(listener);
     }
 
     private static void removeExpired(
@@ -252,7 +303,4 @@ public class SystemStatusMonitor {
                 .currentTimeMillis();
     }
 
-    /*
-     * TODO some sort of listener pattern.
-     */
 }
