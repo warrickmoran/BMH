a169cb88f40e9db7c69dc88ca155c2f6d1a73467
Richard Peter <richard.peter@raytheon.com>
2015-06-18 18:00:19 -0500

    Omaha #4482 - Add tracking of audio processing
    Change-Id: Ibd964ecbe958329c663cbcd7c7610f56e9651bc7

--- cave/com.raytheon.uf.viz.bmh/src/com/raytheon/uf/viz/bmh/ui/dialogs/broadcastcycle/MonitorInlineThread.java
+++ cave/com.raytheon.uf.viz.bmh/src/com/raytheon/uf/viz/bmh/ui/dialogs/broadcastcycle/MonitorInlineThread.java
@@ -137,8 +137,16 @@ public class MonitorInlineThread extends Thread {
                     totalRead += curRead;
                 }
 
+                long t0 = System.currentTimeMillis();
                 logger.packetProcessed();
                 listener.dataArrived(payload);
+                long t1 = System.currentTimeMillis();
+                if (t1 - t0 > 20) {
+                    statusHandler
+                            .debug("Processing of audio packet took "
+                                    + (t1 - t0)
+                                    + "ms.  Consider adding separate thread for audio processing");
+                }
             }
 
             SerializationUtil.transformToThriftUsingStream(
