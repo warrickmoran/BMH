5a596cd58709b00495f2184931ba4bef55dcb94e
David Friedman <David.Friedman@noaa.gov>
2017-05-30 18:49:15 +0000

    ASM #19660 - BMH: Default Java 8 security settings prevent configuration of DAC from CAVE
    Change-Id: I88eba236889e2a757ac17792a93ac761b4d4d67e

--- edex/deploy.edex-BMH/esb/etc/bmh.sh
+++ edex/deploy.edex-BMH/esb/etc/bmh.sh
@@ -6,3 +6,5 @@ export LOG_CONF=logback-bmh.xml
 
 export INIT_MEM=128 # in Meg
 export MAX_MEM=384 # in Meg
+
+export JAVA_SECURITY_OPTION=-Djava.security.properties=/awips2/edex/conf/java.security.allow-md5
